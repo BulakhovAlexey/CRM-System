bf546e34976b3c584a39b7f75a8957e58179d892
BulakhovAlexey <bulakhovl@mail.ru>
2024-06-12 13:48:30 +0400

    fix user profile

--- components/userProfile/index.vue
+++ components/userProfile/index.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="mt-4">
-		<UDivider label="Имя" orientation="horizontal" />
+		<UDivider label="Имя" :orientation="dividerOrientation" />
 		<UserProfileChangeName />
-		<UDivider label="Email" orientation="horizontal" />
+		<UDivider label="Email" :orientation="dividerOrientation" />
 		<UserProfileChangeEmail />
-		<UDivider label="Пароль" orientation="horizontal" />
+		<UDivider label="Пароль" :orientation="dividerOrientation" />
 		<!-- todo  UserProfileChangePassword -->
 	</div>
 </template>
+<script setup lang="ts">
+const dividerOrientation = 'horizontal'
+</script>
